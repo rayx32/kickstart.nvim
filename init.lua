ae5466f7797c3ede19e16a5ac3731ae978eebac1
Sagara Paranagama <sagara89dev@gmail.com>
2024-04-01 20:40:46 +1100

    Add smooth scroll

--- init.lua
+++ init.lua
@@ -847,6 +847,14 @@ require('lazy').setup({
     },
   },
 
+  -- Smooth scroll
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('neoscroll').setup {}
+    end,
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
